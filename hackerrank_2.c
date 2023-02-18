2a377f5349162c37974bbef6743f3f680ebd7548
SaketCSIT <81370601+SaketCSIT@users.noreply.github.com>
2023-02-18 11:35:00 +0530

    Add files via upload

---
+++ hackerrank_2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a;
+    scanf("%d",&a);
+    printf("%d\n",a);
+    printf("%o\n",a);
+    printf("%x\n",a);
+    printf("%X",a);
+    return 0;
+}
